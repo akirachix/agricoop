3733792a6e1068009df495ba1c7970729a45d264
Iman Yahya <Muthoniyahya@gmail.com>
2021-10-21 21:40:29 +0300

    login

--- registration/views.py
+++ registration/views.py
@@ -8,17 +8,17 @@ def login_request(request):
 	if request.method == "POST":
 		form = AuthenticationForm(request, data=request.POST)
 		if form.is_valid():
-			username = form.cleaned_data.get('username')
+			email = form.cleaned_data.get('email')
 			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
+			user = authenticate(email=email, password=password)
 			if user is not None:
 				login(request, user)
-				messages.info(request, f"You are now logged in as {username}.")
+				messages.info(request, f"You are now logged in as {email}.")
 				return redirect("login")
 			else:
-				messages.error(request,"Invalid username or password.")
+				messages.error(request,"Invalid email or password.")
 		else:
-			messages.error(request,"Invalid username or password.")
+			messages.error(request,"Invalid email or password.")
 	form = AuthenticationForm()
 	return render(request=request, template_name="login.html", context={"login_form":form})
 
