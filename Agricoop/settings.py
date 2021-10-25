5e590f77df19109709a5e0269572ef08eb2f0c31
olga20011 <olgaestherakello@gmail.com>
2021-10-25 15:45:48 +0300

    First Deploy

--- Agricoop/settings.py
+++ Agricoop/settings.py
@@ -80,8 +80,6 @@ WSGI_APPLICATION = 'Agricoop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'agricoop',
         'USER': 'farmtoplate',
