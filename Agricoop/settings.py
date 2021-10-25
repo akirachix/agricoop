dd40892ff45f3024648f860359919f59a0dc43e2
olga20011 <olgaestherakello@gmail.com>
2021-10-25 09:29:06 +0300

    signup and login

--- Agricoop/settings.py
+++ Agricoop/settings.py
@@ -131,6 +131,8 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
