ebe87f41085b4d07273e029d706e8fba3daea842
olga20011 <olgaestherakello@gmail.com>
2021-10-26 13:28:33 +0300

    first deployment

--- registration/urls.py
+++ registration/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . views import register_request ,logout_request,login_request,index
 
 urlpatterns = [
-    path("",index,name="home"),
-    path("register/",register_request,name="register"),
+    path("",register_request,name="register"),
     path("login/", login_request, name="login"),
     path("logout/",logout_request, name= "logout"),
    
