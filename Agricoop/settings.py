e26e40628411238d205967409f5738e8f509ccbd
uwasejeannine <jeannineuwasee@gmail.com>
2021-10-27 20:01:17 +0300

    login and signup

--- Agricoop/settings.py
+++ Agricoop/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'registration',
     'whitenoise',
+    # 'groupslist'
 ]
 
 MIDDLEWARE = [
--- Agricoop/urls.py
+++ Agricoop/urls.py
@@ -20,6 +20,6 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('registration.urls')),
-    
+    # path('groupslist/',include('groupslist.urls'))
 
 ]
