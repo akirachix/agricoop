3b35f57acea2a9217779efd7ea96abcc976ff36e
uwasejeannine <jeannineuwasee@gmail.com>
2021-11-03 22:20:29 +0300

    changers on notifications

--- settings/views.py
+++ settings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect,render
 from .models import Setting_page
 from.forms import SettingRegistrationForm
 
@@ -7,6 +7,15 @@ def setting_page(request):
     form= SettingRegistrationForm()
     return render(request,"setting_page.html",{form:"form"})
 
+
 def notifications(request):
-    form= SettingRegistrationForm()
-    return render(request,"notifications.html",{form:"form"})
+    if request.method=="POST":
+        form = SettingRegistrationForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('notifications')
+        else:
+            print(form.errors)
+    else:
+        form= SettingRegistrationForm()
+        return render(request,"notifications.html",{"form":form})
