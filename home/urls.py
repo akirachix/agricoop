256ac705b97388891d540b57b27b07e02fe8203b
Iman Yahya <Muthoniyahya@gmail.com>
2021-11-03 15:42:10 +0300

    grouplist

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import home_page,add_group
+from .views import home_page,add_group, edit, delete
 
 app_name= 'groups'
 
 urlpatterns = [
     path("home/",home_page,name="home_group"),
     path("add/",add_group,name="add_group"),
+    path("edit/<int:id>/",edit,name="edit"),
+    path("delete/<int:id>/",delete,name="delete")
 
 ]
--- home/views.py
+++ home/views.py
@@ -23,3 +23,22 @@ def add_group(request):
     else:
         form=GroupRegistrationForm()
     return render(request,"add_group.html",{"form":form})
+
+def edit(request,id):
+    product=Group_list.objects.get(id=id)
+    if request.method=="POST":
+        form=GroupRegistrationForm(request.POST,instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect(display_groups)
+    else:
+        form=GroupRegistrationForm(instance=product)
+    return render(request,"edit.html",{"form":form})
+
+def delete(request,id):
+    product=Group_list.objects.get(id=id)
+    if request.method=='POST':
+        product.delete()
+        return HttpResponseRedirect(reverse("list"))
+    context={"product":product}
+    return render(request,'delete.html')
