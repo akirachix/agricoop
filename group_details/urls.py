b78796100dbbabd910897a7a955d50dd5eb364ad
uwasejeannine <jeannineuwasee@gmail.com>
2021-11-12 18:15:31 +0300

    commit changes on heroku

--- group_details/urls.py
+++ group_details/urls.py
@@ -5,7 +5,7 @@ from .views import group_details,display_beans,edit,delete,grouped_bens
 app_name = 'group_details'
 urlpatterns = [
         path("group_details/",group_details,name="group_details"),
-        path("group_details/<int:id>/",grouped_bens,name="group_details"),
+        path("grouped_bens",grouped_bens,name="group_details"),
         path("group_details/",display_beans,name="list"),
         path("edit/<int:id>/",edit,name="edit"),
         path("delete/<int:id>/",delete,name="delete")
